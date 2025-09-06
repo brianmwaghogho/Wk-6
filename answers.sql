6c1e6c52e49566bb6469e94ed9431cb56b6d0203
brianmwaghogho33@gmail.com <brianmwaghogho33@gmail.com>
2025-09-06 03:39:36 -0700

    add answers.sql assignment

---
+++ answers.sql
@@ -0,0 +1,25 @@
+-- assignment ; joins and relationships
+
+-- questions 1 -- get FirstName,LastName,emails,officeCode of all employees
+-- use INNER JOIN between employees and offices and ofice code;
+SELECT e.firstName,e.LastName,e.email,o.officeCode
+FROM employees e
+INNER JOIN offices o
+ON e.officeCode = o.officeCode;
+
+-- Question 2 -- Get productName,productVendor, and productLine from products
+-- use LEFT JOIN with productlines on productLine
+
+SELECT p.productName,p.productVendor,pl.productLine
+FROM product p
+LEFT JOIN productionLine pl
+ON p.productionLine = pl.productLine;
+
+-- question 3-- retrieve 
+-- use RIGHT JOINbetween customers and ordes on customersNumber.
+
+SELECT o.orderDate,o.shippedDate,o.status,o.customersNumber
+FROM customers c
+RIGHT JOIN orders o
+ON c.customersNumber = o.customersNumber
+ limit 100;
